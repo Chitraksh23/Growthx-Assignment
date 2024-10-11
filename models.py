2dc8b8aa00cacb51b7460f585ae560f91e6031fb
Chitraksh23 <84462197+Chitraksh23@users.noreply.github.com>
2024-10-11 11:08:58 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,7 +1,1 @@
-from pymongo import MongoClient
-client = MongoClient('mongodb://localhost:27017/')
-db = client['assignment_portal']
-# Collections
-users = db['users']
-admins = db['admins']
-assignments = db['assignments']
+
